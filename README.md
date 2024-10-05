1bccd9cdd2c4bb4af7ada1b14e958d3efeeff26a
Dominik <dominik.reinartz@proton.me>
2024-10-05 19:38:42 +0200

    added basic task execution

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ schematics below (may be changed)
 
 ![image](https://github.com/user-attachments/assets/a2a05848-047d-4d3a-822e-e431287554a5)
 
-Wokwi project: <https://wokwi.com/projects/410665776573350913>
+Wokwi project: <https://wokwi.com/projects/410915727655762945>
--- buttonbox/buttonbox.ino
+++ buttonbox/buttonbox.ino
@@ -2,25 +2,100 @@
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
 
-#define SCREEN_WIDTH 128 // ADJUST set correct width for your OLED
-#define SCREEN_HEIGHT 64 // ADJUST set correct height for your OLED
+using namespace std;
 
-Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
+vector<String> tasks = {};
+vector<int> taskTimestamps = {};
+
+
+void resetDisplay() {
+  display.clearDisplay();
+  display.display();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+}
+
+
+void execTask(String task);
+
 
 void setup() {
   Serial.begin(115200);
   Serial.println("BUTTONBOX - SETUP");
 
-  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("Failed to start SSD1306 OLED"));
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("Failed to start SSD1306 OLED Display"));
     while (1);
   }
 
-  delay(2000);
+  delay(20);
+
+  resetDisplay();
+  display.println("Starting BUTTONBOX...");
+  display.display();
+  delay(200);
+  display.println("Establishing connection with Computer...");
+  display.println("Waiting for HANDSHAKE byte...");
+  display.display();
+  while (true) {
+    Serial.write("HANDSHAKE");
+    if (Serial.available() > 0) {
+      String receivedData = Serial.readStringUntil('\n');
+      if (receivedData == "HANDSHAKE") {
+        break;
+      }
+    }
+    delay(10);
+  }
+  display.println("HANDSHAKE received.");
+  display.display();
 }
 
+
 void loop() {
-  oled.clearDisplay();
-  oled.drawTriangle(30, 50, 98, 50, 64, 10, WHITE);
-  oled.display();
+  while (true) {
+    if (Serial.available() > 0) {
+      String receivedData = Serial.readStringUntil('\n');
+      execTask(receivedData);
+    }
+  }
+}
+
+
+void execDisplayTask(String task) {
+  if (task.startsWith("display reset")) {
+    resetDisplay();
+  } else if (task.startsWith("display display")) {
+    display.display();
+  } else if (task.startsWith("display print")) {
+    String text = task.substring(14);
+    display.print(text);
+  } else if (task.startsWith("display println")) {
+    String text = task.substring(16);
+    display.print(text);
+  } else {
+    Serial.println("ERROR Invalid display task '" + task + "'");
+  }
+}
+
+
+void execTask(String task) {
+  if (task.startsWith("digital HIGH")) {
+    int num = task.substring(13).toInt();
+    digitalWrite(num, HIGH);
+  } else if (task.startsWith("digital LOW")) {
+    int num = task.substring(12).toInt();
+    digitalWrite(num, LOW);
+  } else if (task.startsWith("display")) {
+    execDisplayTask(task);
+  } else {
+    Serial.println("ERROR Invalid task '" + task + "'");
+  }
 }
