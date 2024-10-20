5e5e50f712646cd2ab5ede36bfb6376fd8695d4a
Dominik <dominik.reinartz@proton.me>
2024-10-20 15:43:44 +0200

    Windows compile_icons.ps1 script now auto-reencodes the resource.py file as utf-8

--- client/compile_icons.ps1
+++ client/compile_icons.ps1
@@ -1,2 +1,4 @@
 # Must be run from an environment with pyside6 installed
 pyside6-rcc client/buttonbox_client/icons/menu_icons.qrc | ForEach-Object { $_ -replace 'PySide6', 'PyQt6' } > client/buttonbox_client/icons/resource.py
+# Re-encode as utf-8 as pyside6-rcc tends to encode it as Macintosh (CR)
+Get-Content client/buttonbox_client/icons/resource.py | Out-File -Encoding utf8 -Filepath client/buttonbox_client/icons/resource.py
